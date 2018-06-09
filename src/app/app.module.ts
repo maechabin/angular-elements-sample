28f91e74c79a3bcc385f2a580559d6c47a32560f
maechabin <mitoppo@gmail.com>
2018-06-10 03:46:27 +0900

    boot strapingをapp.componentで行うように修正

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
-import { NgModule, Injector } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  entryComponents: [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap: [AppComponent],
+  entryComponents: [ HelloComponent ]
 })
-export class AppModule {
-  constructor(
-    private injector: Injector,
-  ) { }
-  ngDoBootstrap() {
-    const AppElement = createCustomElement(AppComponent, { injector: this.injector });
-    customElements.define('app-hello', AppElement);
-  }
-}
+export class AppModule { }
